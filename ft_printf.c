2b5d3b944060479f1c7180148dcab3004ece843d
JoanLonsan <joancres12@gmail.com>
2022-10-17 20:33:07 +0200

    Upload 42

--- ft_printf.c
+++ ft_printf.c
@@ -3,74 +3,71 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jcrescen <jcrescen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/02 21:52:49 by marvin            #+#    #+#             */
-/*   Updated: 2022/10/02 21:52:49 by marvin           ###   ########.fr       */
+/*   Created: 2022/10/17 19:24:10 by jcrescen          #+#    #+#             */
+/*   Updated: 2022/10/17 19:24:10 by jcrescen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int     ft_spcount(t_printf tab)
+int	ft_spcount(t_printf tab)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while(*tab->str)
-    {
-        if (*tab->str == '%')
-        {
-            ++i;
-            if (*tab->str + 1 == '%')
-                +++tab->str;
-            else
-                ++tab->str;
-        }
-        else
-            ++tab->str;
-    }
-    return (i);
+	i = 0;
+	while (*tab->str)
+	{
+		if (*tab->str == '%')
+		{
+			++i;
+			++tab->str;
+		}
+		else
+			++tab->str;
+	}
+	return (i);
 }
 
 t_print *ft_init_tab(t_print *tab)
 {
-    tab->spcount = 0;
-    tab->width = 0;
-    tab->zero = 0;
-    tab->point = 0;
-    tab->dash = 0;
-    tab->tlen = 0;
-    tab->sign = 0;
-    tab->porc = 0;
-    tab->space = 0;
-    return (tab);
+	tab->spcount = 0;
+	tab->width = 0;
+	tab->zero = 0;
+	tab->point = 0;
+	tab->dash = 0;
+	tab->tlen = 0;
+	tab->sign = 0;
+	tab->porc = 0;
+	tab->space = 0;
+	return (tab);
 }
 
-void    ft_str_printf(t_printf tab)
+void	ft_str_printf(t_printf tab)
 {
-    tab->tlen = 0;
-    while (*tab->str)
-    {
-        if (*tab->str == '%')
-        {
-            ft_lfsp_str(&tab);
-        }
-        else
-            tab->tlen += write(1, p->str, 1);
-        ++tab->str;
-    }
+	tab->tlen = 0;
+	while (*tab->str)
+	{
+		if (*tab->str == '%')
+		{
+			ft_lfsp_str(&tab);
+		}
+		else
+			tab->tlen += write(1, p->str, 1);
+		++tab->str;
+	}
 }
 
-int ft_printf(const char *str, ...)
+int	ft_printf(const char *str, ...)
 {
-   t_printf  tab;
+	t_printf  tab;
 
-   tab->str = (char *)str;
-   tab->spcount = ft_spcount(tab);
-   va_start(tab->args);
-   ft_init_tab(tab);
-   ft_str_printf(&tab);
-   va_end(tab->args)
-   return (tab->tlen);
+	tab->str = (char *)str;
+	tab->spcount = ft_spcount(tab);
+	va_start(tab->args);
+	ft_init_tab(tab);
+	ft_str_printf(&tab);
+	va_end(tab->args)
+	return (tab->tlen);
 }
--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -11,26 +11,25 @@
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+# define FT_PRINTF_H
+# include "../libft/libft.h"
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-#include "../libft/libft.h"
-#include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-typedef struct  s_printf
+typedef struct s_printf
 {
-    int     spcount;
-    int     width;
-    char    zero;
-    char    point;
-    char    dash;
-    int     tlen;
-    char    sign;
-    char    porc;
-    char    space;
-    va_list args;
-}               t_printf;
+	int		spcount;
+	int		width;
+	char	zero;
+	char	point;
+	char	dash;
+	int		tlen;
+	char	sign;
+	char	porc;
+	char	space;
+	va_list	args;
+}				t_printf;
 
-int     ft_printf(const char *, ...);
-int     ft_lfsp_str(t_print *tab);
+int		ft_printf(const char *, ...);
+int		ft_lfsp_str(t_print *tab);
